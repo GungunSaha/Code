dd27913cef32bb6514bae0e8b4569b6e0f6ca763
GungunSaha <sahagungun0401@gmail.com>
2026-05-06 22:52:27 +0530

    Update yt1.cpp

--- yt1.cpp
+++ yt1.cpp
@@ -1,8 +1,6 @@
 #include<iostream>
 using namespace std;
 int main() {
-   cout << "MY NAME IS GUNGUN SAHA " ;
-   cout << endl;
-   cout << "my name is gungun " ;
+   cout << "MY NAME IS GUNGUN SAHA " << endl;
 return 0; 
 }
