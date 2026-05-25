1a205bb4d362d77a895e123bec62b6819d168e36
GungunSaha <sahagungun0401@gmail.com>
2026-05-25 20:29:42 +0530

    Add files via upload

---
+++ yt2 a .cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int budget ;
+cout << "Enter your budget " << endl ;
+cin>> budget ;
+ if ( budget > 2000000) { 
+  cout << "you can buy scarpio" << endl ;
+     }
+else {
+    cout << "you can not buy scorpio" << endl ;
+}
+return 0;
+
+}
