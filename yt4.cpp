bf0dcfab158aac34d5df95d8cc93ee713872253e
GungunSaha <sahagungun0401@gmail.com>
2026-05-26 20:47:25 +0530

    ternary operator

---
+++ yt4.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+int main (){
+
+    int age;
+    cin>> age;
+    (age > 18 ) ? cout<< "You can vote " : cout<< "You can not vote " ;
+
+
+    return 0;
+}
